dfefafb70fe9e5c8d1f4aafea919c0f0316c4771
Marcosjuunioor <marcosjuunioor2@gmail.com>
2020-04-04 19:35:38 -0300

    bootstrap

--- application/views/contas.php
+++ application/views/contas.php
@@ -4,10 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Contas Bancárias</title>
+    <title>Contas Bancárias</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost/ci/desafio-bisa/assets/css/contas.css">
 </head>
 <body>
-    <h1>Lista de contas: </h1>
+    <h1 class="titulo"><span class="badge badge-secondary">Lista de Contas Bancárias</span> </h1>
     <div>
         <?php 
             if(!empty($contas)){
@@ -16,36 +18,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     $descricao = $conta->descricao;
                     $saldo = $conta->saldo;
                     echo "
-                    <form method='post' action='http://localhost/ci/desafio-bisa/index.php/conta_control/atualizar_conta/$id_conta'>
-                        <label for='id_descricao'>Descrição</label>
-                        <input type='text' id='id_descricao' name='descricao' value=$descricao>
-                        <label for='id_saldo'>Saldo</label>
-                        <input type='number' id='id_saldo' name='saldo' value=$saldo>
-                        <input type='submit' value='atualizar'/>
-                        <a href='http://localhost/ci/desafio-bisa/index.php/conta_control/excluir_conta/$id_conta'> <input type='button' value='deletar' id='id_deletar'/></a>
-                        <a href='http://localhost/ci/desafio-bisa/index.php/movimentacao_control/listar_movimentacoes/$id_conta'> <input type='button' value='movimentações' id='id_movimentacoes'/></a><br>
-                    </form>
+                    <div class='forms'>
+                        <form method='post' action='http://localhost/ci/desafio-bisa/index.php/conta_control/atualizar_conta/$id_conta'>
+                            <div class='form-row'>
+                                <div class='col'>
+                                    <label for='id_descricao'>Descrição</label>
+                                    <input type='text' id='id_descricao' name='descricao' value=$descricao required/>    
+
+                                    <label for='id_saldo'>Saldo</label>
+                                    <input type='number' id='id_saldo' name='saldo' value=$saldo required/>
+
+                                    <input type='submit' value='atualizar' class='btn btn-success'/>
+                                    <a href='http://localhost/ci/desafio-bisa/index.php/conta_control/excluir_conta/$id_conta'> <input type='button' value='deletar' id='id_deletar' class='btn btn-danger'/></a>
+                                    <a href='http://localhost/ci/desafio-bisa/index.php/movimentacao_control/listar_movimentacoes/$id_conta'> <input type='button' value='movimentações' id='id_movimentacoes' class='btn btn-info'/></a><br>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                     ";      
                     
                 }
             }else{
-                echo "<h2>Não há contas cadastradas!</h2>";
+                echo "<h2 class='titulo'><span class='badge badge-light'>Não há contas cadastradas!</span></h2>";
             }
         ?>
         
-
-        <form method="post" action="http://localhost/ci/desafio-bisa/index.php/conta_control/criar_conta">
-            <label for="id_descricao">Descrição</label><br>
-            <input type="text" id="id_descricao" name="descricao"><br>
-            <label for="id_saldo">Saldo</label><br>
-            <input type="number" id="id_saldo" name="saldo"><br>
-            <input type="submit" value="cadastrar"/>
-        </form>
+        <div class="forms"> 
+            <form method="post" action="http://localhost/ci/desafio-bisa/index.php/conta_control/criar_conta">
+                <div class="form-row">
+                    <div class='col'>
+                        <label for="id_descricao">Descrição</label>
+                        <input type="text" id="id_descricao" name="descricao" required/>
+                
+                        <label for="id_saldo">Saldo</label>
+                        <input type="number" id="id_saldo" name="saldo" required/>
+                
+                        <input type="submit" value="cadastrar" class="btn btn-primary"/>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="http://localhost/ci/desafio-bisa/assets/javascript/contas.js"></script>
 </body>
-
-
 </html>
 
